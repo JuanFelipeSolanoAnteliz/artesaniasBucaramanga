045cf5e4b56265639b04e60e1b6bdcc7dc13c9f7
JuanFelipeSolanoAnteliz <juanfesolant0906@gmail.com>
2024-11-05 09:28:40 -0500

    feat: :tada: basic settings

---
+++ app.js
@@ -0,0 +1,25 @@
+const indexRouter = require('./server/views/indexRouter'); 
+const express = require('express');
+const { join } = require('path');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(join(__dirname, 'client/dist')));
+
+
+// app.get('*', (req, res) => {
+//     res.sendFile(join(__dirname, 'client/dist/index.html'));
+// });
+
+let config = {
+    port: process.env.EXPRESS_PORT || 5000,
+    host: process.env.EXPRESS_HOST_NAME || 'localhost'
+};
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+})
+
+app.listen(config, () => {
+    console.log(`Server running at http://${config.host}:${config.port}`);
+});
