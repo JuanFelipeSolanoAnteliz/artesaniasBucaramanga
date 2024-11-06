7b74d906cbd9c7d63fe6cc14611a8238139300be
JuanFelipeSolanoAnteliz <juanfesolant0906>
2024-11-06 08:26:23 -0500

    feat: :goal_net: error handling with conditionals and 'try catch' blocks

--- server/controller/productsController.js
+++ server/controller/productsController.js
@@ -4,7 +4,7 @@ const Products = require('../model/productsModel');
 exports.getAllProducts = async (req, res) =>{
     try{
         let result = await Products.find();
-        
+        if(result.length === 0)  res.status(404).send({'status':404,'message':'there is no products to fecth'});
         res.status(200).send({'status':200,'message':'products fetched correctly', 'data':result});
     }catch(error){
         console.log(error)
