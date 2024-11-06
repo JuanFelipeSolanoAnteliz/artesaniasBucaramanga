b89c588989a1c997eaac6e78809ecfae6bbe90b9
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-11-06 07:24:05 -0500

    fix: :hammer: structure in app

--- app.js
+++ app.js
@@ -1,33 +1,26 @@
-const indexRouter = require('./server/views/indexRouter'); 
+const indexRouter = require('./server/views/indexRouter');
 const express = require('express');
+const { connect } = require('http2');
 const { join } = require('path');
-import connectDB from './server/helper/connect';
-
-
-
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(join(__dirname, 'client/dist')));
 
 
-// app.get('*', (req, res) => {
-//     res.sendFile(join(__dirname, 'client/dist/index.html'));
-// });
+app.use("/", indexRouter);
+
+
+app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, 'client/dist/index.html'));
+});
 
 let config = {
     port: process.env.EXPRESS_PORT || 5000,
     host: process.env.EXPRESS_HOST_NAME || 'localhost'
 };
 
-
-// Conexión a MongoDB
-connectDB(); // Conecta a MongoDB
-
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-})
 
 app.listen(config, () => {
     console.log(`Server running at http://${config.host}:${config.port}`);
